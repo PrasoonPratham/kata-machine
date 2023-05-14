5d6c26ad0264a4fc850667fbc18e57d1a0b583e7
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2023-05-15 02:21:35 +0530

    Binary search completed

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let high, low, mid;
+    low = 0;
+    high = haystack.length - 1;
+    while (low <= high) {
+        mid = Math.floor((high + low) / 2);
+        if (haystack[mid] < needle) {
+            low = mid + 1;
+        } else if (haystack[mid] > needle) {
+            high = mid - 1;
+        } else {
+            return true;
+        }
+    }
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let step = 0;
+    step = Math.floor(Math.sqrt(breaks.length))
+    
+    while 
+    if (breaks[step] != true) {
+        step = step + step
+    }
 
+    function linear_search(arr: boolean[], start: number, end: number) {
+        while (start <= end) {
+            if (arr[start] === false) {
+                start += 1;
+            }
+            if (arr[start] === true) {
+                return true;
+            }        
+        }
+        return false;
+    }
 }

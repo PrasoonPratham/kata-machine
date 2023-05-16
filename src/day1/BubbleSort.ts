88bb601670b87a149ddf6822c288d65096f06ac8
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2023-05-16 19:36:38 +0530

    Bubblesort implemented

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,15 @@
 export default function bubble_sort(arr: number[]): void {
-
+    let temp: number = 0;
+    let swap: boolean;
+    do {
+        swap = false;
+        for (let index = 0; index < arr.length - 1; index++) {
+            if (arr[index] > arr[index + 1]) {
+                temp = arr[index];
+                arr[index] = arr[index + 1];
+                arr[index + 1] = temp;
+                swap = true;
+            }
+        }
+    } while(swap)
 }
